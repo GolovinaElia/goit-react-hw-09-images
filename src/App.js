2f937f2ffad818b8ce5d0f8364fde5894ba77e7c
GolovinaElia <eliagolovina@ukr.net>
2021-06-19 18:17:24 +0300

    Добавляет файлы

--- src/App.js
+++ src/App.js
@@ -5,7 +5,90 @@ import Searchbar from './components/Searchbar';
 import ImageGallery from './components/ImageGallery';
 import Button from './components/Button';
 import Modal from './components/Modal';
-import fetchHits from './services/api';
+import apiFetchHits from './services/api';
+
+// export default function App() {
+//   const [hits, setHits] = useState([]);
+//   const [currentPage, setCurrentPage] = useState(1);
+//   const [searchQuery, setSearchQuery] = useState('');
+//   const [showModal, setShowModal] = useState(false);
+//   const [largeImageURL, setlargeImageURL] = useState('');
+//   const [isLoading, setIsLoading] = useState(false);
+//   const [error, setError] = useState(null);
+
+//   const onChangeQuery = query => {
+//     setSearchQuery({ searchQuery: query });
+//     setCurrentPage(currentPage);
+//     setHits(hits);
+//     setError(error);
+//   };
+
+//   const fetchHits = () => {
+//     const options = { currentPage, searchQuery };
+
+//     setIsLoading({ isLoading: true });
+
+//     apiFetchHits(options)
+//       .then(hits =>
+//         this.setState(prevState => ({
+//           hits: [...prevState.hits, ...hits],
+//           currentPage: prevState.currentPage + 1,
+//         })),
+//       )
+//       .catch(error => this.setState({ error }))
+//       .finally(() => this.setState({ isLoading: false }));
+//   };
+
+//   const scrollBtn = () => {
+//     window.scrollTo({
+//       top: document.documentElement.scrollHeight,
+//       behavior: 'smooth',
+//     });
+//   };
+
+//   useEffect(() => {
+//     setTimeout(() => {
+//       scrollBtn();
+//     }, 1000);
+//   }, []);
+
+//   useEffect(() => {
+//     setSearchQuery();
+//   }, []);
+
+//   const toggleModal = () => {
+//     setShowModal(({ showModal }) => ({
+//       showModal: !showModal,
+//     }));
+//   };
+
+//   const handleClickImg = url => {
+//     setlargeImageURL({ largeImageURL: url });
+//     toggleModal();
+//   };
+
+//   const renderButton = hits.length > 0 && !isLoading;
+
+//   return (
+//     <>
+//       {error && <h1>404 error: File not found</h1>}
+
+//       <Searchbar onSubmit={onChangeQuery} />
+
+//       {isLoading && <Loader />}
+
+//       <ImageGallery hits={hits} imgModal={handleClickImg} />
+
+//       {renderButton && <Button onClick={fetchHits} />}
+
+//       {showModal && (
+//         <Modal onClose={toggleModal}>
+//           <img src={largeImageURL} alt="" />
+//         </Modal>
+//       )}
+//     </>
+//   );
+// }
 
 class App extends Component {
   state = {
@@ -42,7 +125,7 @@ class App extends Component {
 
     this.setState({ isLoading: true });
 
-    fetchHits(options)
+    apiFetchHits(options)
       .then(hits =>
         this.setState(prevState => ({
           hits: [...prevState.hits, ...hits],
--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -1,6 +1,40 @@
 import React, { Component } from 'react';
 import style from './Searchbar.module.css';
 
+// export default function Searchbar() {
+//   const [query, setQuery] = useState('');
+
+//   const handleSubmit = event => {
+//     event.preventDefault();
+//     this.props.onSubmit(query);
+//     setQuery({ query: '' });
+//   };
+
+//   const handleChange = event => {
+//     setQuery(event.currentTarget.value);
+//   };
+
+//   return (
+//     <header className={style.Searchbar}>
+//       <form className={style.SearchForm} onSubmit={handleSubmit}>
+//         <button type="submit" className={style.SearchFormButton}>
+//           <span className={style.SearchFormButtonLabel}>Search</span>
+//         </button>
+
+//         <input
+//           className={style.SearchFormInput}
+//           type="text"
+//           autoComplete="off"
+//           autoFocus
+//           placeholder="Search images and photos"
+//           value={query}
+//           onChange={handleChange}
+//         />
+//       </form>
+//     </header>
+//   );
+// }
+
 class Searchbar extends Component {
   state = {
     query: '',
